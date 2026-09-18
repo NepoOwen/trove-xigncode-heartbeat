00c415a159b600a10bb0466b5ea9be47c3444fc0
NepoOwen <linus.v.sjo@gmail.com>
2026-09-18 17:07:14 +0200

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,19 @@
+// File: main.hpp
+// Author: NepoOwen
+// https://github.com/NepoOwen/trove-xigncode-heartbeat
+
+#include "challenge.hpp"
+#include <cstdint>
+
+DWORD WINAPI SetupThread(LPVOID lpParam) {
+	auto t = detail::client::helper::freeze(); // freeze
+    xigncode::initialize(); // xigncode
+	detail::client::helper::unfreeze(t); // unfreeze
+    return 0;
+}
+
+extern "C" __declspec(dllexport) BOOL WINAPI clientdll(HMODULE hModule, void* config = nullptr) {
+	HANDLE thread = CreateThread(nullptr, 0, SetupThread, nullptr, 0, nullptr);
+	if (thread) { CloseHandle(thread); return 1; }
+	return 0;
+}
